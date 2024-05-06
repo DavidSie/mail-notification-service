9a22bd60ef723f3acbc2f60dc644b69a27fff729
David Sieciński <david.siecinski@goldenoakit.pl>
2024-05-06 10:52:52 +0200

    feat: Send mail via  smtp server configurd in config file

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -1,16 +1,62 @@
 package mail
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/DavidSie/notification-service/pkg/model"
+	mail "github.com/xhit/go-simple-mail/v2"
 )
 
-type Mailer struct{}
+type Mailer struct {
+	AppConfig model.AppConfig
+}
+
+func (m Mailer) ListenForMail() {
+	go func() {
+		for {
+			msg := <-m.AppConfig.MailChannel
+			err := m.sendMail(msg)
+			if err != nil {
+				log.Printf("Error while sending mail %v \n", err)
+			}
+		}
+
+	}()
+
+}
 
 func (m Mailer) Send(emailRequest model.EmailRequest) error {
+	return m.sendMail(emailRequest)
+}
 
-	fmt.Printf("Message send to %s title: %s \n ", emailRequest.Recipients, emailRequest.Title)
+func (m Mailer) sendMail(emailRequest model.EmailRequest) error {
+	server := mail.NewSMTPClient()
+	server.Host = m.AppConfig.Stmp.Host
+	server.Port = m.AppConfig.Stmp.Port
+	server.Authentication = m.AppConfig.Stmp.Authentication
+	server.Username = m.AppConfig.Stmp.Username
+	server.Password = m.AppConfig.Stmp.Password
+	server.TLSConfig = m.AppConfig.Stmp.TLSConfig
+	server.Encryption = m.AppConfig.Stmp.Encryption
+	server.KeepAlive = m.AppConfig.Stmp.KeepAlive
+	server.ConnectTimeout = m.AppConfig.Stmp.ConnectTimeout
+	server.SendTimeout = m.AppConfig.Stmp.SendTimeout
+
+	client, err := server.Connect()
+
+	if err != nil {
+		return err
+	}
+	email := mail.NewMSG()
+	email.SetFrom(emailRequest.Sender).AddTo(emailRequest.Recipients...).AddCc(emailRequest.CcRecipients...).AddBcc(emailRequest.BccRecipients...).SetSubject(emailRequest.Title)
+	email.SetBody(emailRequest.MessageContentType, emailRequest.Message)
+	err = email.Send(client)
+
+	if err != nil {
+		return err
+	} else {
+		log.Printf("Email to %s sent! \n", emailRequest.Recipients[0])
+	}
+
 	return nil
-	//auth smtp.PlainAuth()
 }
